12f81b7c5a27c540204e32cb294a29f57bc0ed4c
kashishInamdar <kashishinamdar2004@gmail.com>
2024-12-14 18:54:52 +0530

    signup authntication

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import dotenv from "dotenv";
-import authroutes from "./routes/auth.routes.js"
+
+import authRoutes from "./routes/auth.routes.js"
 import connectToMongoDB from './db/connectToMongoDB.js';
-dotenv.config();
+
 
 const app = express();
-
-
-app.get("/",(req , res)=>{
-    res.send("Hello Server I am Kashish form Soneki chidya")
-})
-
-app.use("/api/auth", authroutes);
-
 const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // to parese the incoming requests with JSON payloads (from req.body).
+app.use("/api/auth", authRoutes);
+
+// app.get("/",(req , res)=>{
+//     res.send("Hello Server")
+// })
+
 app.listen(PORT , ()=>{
     console.log(`server Running 🏃‍♀️ on ${PORT} port.`)
     connectToMongoDB();
