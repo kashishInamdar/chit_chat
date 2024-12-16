c5dc5ad7c22ff4a8b3070a5aff3ba8d21b37fce8
kashishInamdar <kashishinamdar2004@gmail.com>
2024-12-16 15:35:14 +0530

    Login authontication done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -33,7 +33,7 @@ export const signup = async(req ,res)=>{
         })
 
         if(newUser){
-            // Generate JWT token hear ;
+            // Generate JWT token hear ; 
             generateTokenAndSetCookie(newUser._id, res);
             await newUser.save();
 
@@ -54,8 +54,32 @@ export const signup = async(req ,res)=>{
    }
 };
 
-export const login = (req ,res)=>{
-    console.log("Login User");
+export const login = async(req ,res)=>{
+    try{
+        const {userName , password} = req.body;
+
+        // Fetch user from database
+        const user = await User.findOne({userName});
+        const isPasswordCorrect = await bcrypt.compare(password , user?.password || "");
+    
+        // Check if user exists and password is correct
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error : "Invalid username or password"});
+        }
+        
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id : user._id,
+            fullName : user.fullName,
+            userName : user.userName,
+            profilePic : user.profilePic,
+        })
+        
+    }catch(error){
+    console.log("Error on Login controller", error.message);
+    res.status(500).json({ error : "Internal Server Error"});
+   }
 }
 
 export const logout = (req ,res)=>{
